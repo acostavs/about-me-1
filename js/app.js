d4c3c23c135143470411c18ec4d3c6130becc74b
acostavs <acostavs1015@gmail.com>
2023-01-26 23:03:34 +0000

    updated functions

--- js/app.js
+++ js/app.js
@@ -9,44 +9,58 @@ function getUserName() {
 // Get user name
 
 // Question 1:
-let userResponse1 = prompt('Have I ever lived in another Country?');
-if (userResponse1 === 'yes' || userResponse1 === 'y') {
-  alert('Good job, ' + userName + ' That is correct!');
-} else {
-  //console.log('Sorry, ' + userName + '. That is not correct.');
-  alert('Sorry, ' + userName + '. That is not correct.');
+function qOne() {
+  let userResponse1 = prompt('Have I ever lived in another Country?');
+  if (userResponse1 === 'yes' || userResponse1 === 'y') {
+    alert('Good job, ' + userName + ' That is correct!');
+  } else {
+    //console.log('Sorry, ' + userName + '. That is not correct.');
+    alert('Sorry, ' + userName + '. That is not correct.');
+  }
 }
 
 // Question 2:
-let userResponse2 = prompt('Do I coach baseball?');
-if (userResponse2 === 'yes' || userResponse2 === 'y') {
-  alert('Good job, ' + userName + ' That is correct!');
-} else {
-  //console.log('Sorry, ' + userName + '. That is not correct.');
-  alert('Sorry, ' + userName + '. That is not correct.');
+function qTwo() {
+
+  let userResponse2 = prompt('Do I coach baseball?');
+  if (userResponse2 === 'yes' || userResponse2 === 'y') {
+    alert('Good job, ' + userName + ' That is correct!');
+  } else {
+    //console.log('Sorry, ' + userName + '. That is not correct.');
+    alert('Sorry, ' + userName + '. That is not correct.');
+  }
 }
 // Question 3:
-let userResponse3 = prompt('Was I in the military?');
-if (userResponse3 === 'yes' || userResponse3 === 'y') {
-  alert('Good job, ' + userName + ' That is correct!');
-} else {
-  //console.log('Sorry, ' + userName + '. That is not correct.');
-  alert('Sorry, ' + userName + '. That is not correct.');
+function qThree() {
+
+  let userResponse3 = prompt('Was I in the military?');
+  if (userResponse3 === 'yes' || userResponse3 === 'y') {
+    alert('Good job, ' + userName + ' That is correct!');
+  } else {
+    //console.log('Sorry, ' + userName + '. That is not correct.');
+    alert('Sorry, ' + userName + '. That is not correct.');
+  }
 }
 // Question 4:
-let userResponse4 = prompt('Have I ever lived in Canada?');
-if (userResponse4 === 'no' || userResponse4 === 'n') {
-  alert('Good job, ' + userName + ' That is correct!');
-} else {
-  ///console.log('Sorry, ' + userName + '. That is not correct.');
-  alert('Sorry, ' + userName + '. That is not correct.');
+function qFour() {
+
+  let userResponse4 = prompt('Have I ever lived in Canada?');
+  if (userResponse4 === 'no' || userResponse4 === 'n') {
+    alert('Good job, ' + userName + ' That is correct!');
+  } else {
+    ///console.log('Sorry, ' + userName + '. That is not correct.');
+    alert('Sorry, ' + userName + '. That is not correct.');
+  }
 }
 // Question 5:
-let userResponse5 = prompt('Do I like programming?');
-if (userResponse5 === 'yes' || userResponse5 === 'y') {
-  alert('Good job, ' + userName + ' That is correct!');
-} else {
-  //console.log('Sorry, ' + userName + '. That is not correct.');
-  alert('Sorry, ' + userName + '. That is not correct.');
+function qFive() {
+
+  let userResponse5 = prompt('Do I like programming?');
+  if (userResponse5 === 'yes' || userResponse5 === 'y') {
+    alert('Good job, ' + userName + ' That is correct!');
+  } else {
+    //console.log('Sorry, ' + userName + '. That is not correct.');
+    alert('Sorry, ' + userName + '. That is not correct.');
+  }
 }
 
